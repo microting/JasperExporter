a7433a6969ee1cb21feaf5a640f0611670343cc9
Alexander <akarabelnikov@gmail.com>
2018-06-10 19:44:05 +0300

    add tests info

--- src/test/java/com/microting/report/jasper/NegativeArgumentsTest.java
+++ src/test/java/com/microting/report/jasper/NegativeArgumentsTest.java
@@ -10,18 +10,18 @@ public class NegativeArgumentsTest {
 	@Test
 	public void testArguments_WrongNumberOfArguments_Failed() {
 		JasperExporter endpoint = new JasperExporter("wrong_argument");
-		assertEquals(endpoint.buildReport(), ExitCode.WRONG_ARGUMENTS.getCode());
+		assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
 	}
 
 	@Test
 	public void testArguments_WrongArgumentDeclaration_Failed() {
 		JasperExporter endpoint = new JasperExporter("arg1", "arg2", "arg3", "arg4");
-		assertEquals(endpoint.buildReport(), ExitCode.WRONG_ARGUMENTS.getCode());
+		assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
 	}
 
 	@Test
 	public void testArguments_UnsupportedArgument_Failed() {
 		JasperExporter endpoint = new JasperExporter("-arg1=agrVal1", "-arg2=agrVal2", "-arg3=agrVal3", "-arg4=argVal4");
-		assertEquals(endpoint.buildReport(), ExitCode.WRONG_ARGUMENTS.getCode());
+		assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
 	}
 }
