513701729c33f2e0fec8a21ff3d7bdadd5faf0ff
akarabelnikov <akarabelnikov@users.noreply.github.com>
2022-01-06 19:07:57 +0300

    Up gradle (#11)
    Up gradle version and some libraries

--- service/src/test/java/com/microting/report/jasper/NegativeArgumentsTest.java
+++ service/src/test/java/com/microting/report/jasper/NegativeArgumentsTest.java
@@ -1,27 +1,27 @@
 package com.microting.report.jasper;
 
 import com.microting.report.jasper.JasperExporter.ExitCode;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NegativeArgumentsTest {
 
-	@Test
-	public void testArguments_WrongNumberOfArguments_Failed() {
-		JasperExporter endpoint = new JasperExporter("wrong_argument");
-		assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
-	}
+    @Test
+    public void testArguments_WrongNumberOfArguments_Failed() {
+        JasperExporter endpoint = new JasperExporter("wrong_argument");
+        assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
+    }
 
-	@Test
-	public void testArguments_WrongArgumentDeclaration_Failed() {
-		JasperExporter endpoint = new JasperExporter("arg1", "arg2", "arg3", "arg4");
-		assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
-	}
+    @Test
+    public void testArguments_WrongArgumentDeclaration_Failed() {
+        JasperExporter endpoint = new JasperExporter("arg1", "arg2", "arg3", "arg4");
+        assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
+    }
 
-	@Test
-	public void testArguments_UnsupportedArgument_Failed() {
-		JasperExporter endpoint = new JasperExporter("-arg1=agrVal1", "-arg2=agrVal2", "-arg3=agrVal3", "-arg4=argVal4");
-		assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
-	}
+    @Test
+    public void testArguments_UnsupportedArgument_Failed() {
+        JasperExporter endpoint = new JasperExporter("-arg1=agrVal1", "-arg2=agrVal2", "-arg3=agrVal3", "-arg4=argVal4");
+        assertEquals(ExitCode.WRONG_ARGUMENTS.getCode(), endpoint.buildReport());
+    }
 }
